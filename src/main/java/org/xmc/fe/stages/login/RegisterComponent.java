3a62cb9a45cb730147905ccc0dfe70fd3b1897be
MatWein <hm78xqKyvJB7wpl8KaskImydeObsj0G0>
2020-05-24 19:53:16 +0200

    .

--- src/main/java/org/xmc/fe/stages/login/RegisterComponent.java
+++ src/main/java/org/xmc/fe/stages/login/RegisterComponent.java
@@ -1,7 +1,6 @@
 package org.xmc.fe.stages.login;
 
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -14,7 +13,6 @@ public class RegisterComponent {
     @FXML public TextField displayNameTextfield;
     @FXML public TextField usernameTextfield;
     @FXML public PasswordField passwordField1;
-    @FXML public PasswordField passwordField2;
 
     @FXML
     public void onBack() {
@@ -26,8 +24,6 @@ public class RegisterComponent {
 
     @FXML
     public void onRegister() {
-        for (Node node : usernameTextfield.getScene().getRoot().getChildrenUnmodifiable()) {
-            System.out.println(node.getProperties());
-        }
+
     }
 }

a18aeca297f629de820328dae41603fd506915d4
MatWein <matthiasweinert@web.de>
2021-02-21 09:27:26 +0100

    added first analysis fxml

---
+++ src/main/java/org/xmc/fe/stages/main/analysis/AnalysisController.java
@@ -0,0 +1,23 @@
+package org.xmc.fe.stages.main.analysis;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import org.xmc.fe.ui.FxmlController;
+
+@FxmlController
+public class AnalysisController {
+	@FXML private MenuButton favouriteMenuButton;
+	
+	@FXML
+	public void initialize() {
+	
+	}
+	
+	@FXML
+	public void clearInput() {
+	}
+	
+	@FXML
+	public void saveAsFavourite() {
+	}
+}
